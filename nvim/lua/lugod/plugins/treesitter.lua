aa1a516c9d428e491a62c62e16e59608fd94cf43
Waleed Lugod <lugodwaleed@gmail.con>
2024-02-16 18:28:56 +0800

    Fixed treesitter parsers

--- nvim/lua/lugod/plugins/treesitter.lua
+++ nvim/lua/lugod/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
 		config = function()
 			require'nvim-treesitter.configs'.setup {
 				-- A list of parser names, or "all" (the five listed parsers should always be installed)
-				ensure_installed = { "c", "help", "lua", "vim", "vimdoc", "query" },
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
 
 				-- Install parsers synchronously (only applied to `ensure_installed`)
 				sync_install = false,
